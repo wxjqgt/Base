ec5c21ddb3a1b21a13117cb51be66a45cfdcaea7
weibo <gt1254094162@163.com>
2017-03-06 12:10:00 +0800

    这是一个自定义基类库-20170306_12.08

---
+++ app/src/main/java/com/weibo/base/view/BaseFragment.java
@@ -0,0 +1,48 @@
+package com.weibo.base.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.weibo.base.util.ViewUtil;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public abstract class BaseFragment extends Fragment {
+
+    private View view;
+
+    public BaseFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(getLayoutId(),container,false);
+        initView();
+        loadData();
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser && view != null){
+            loadData();
+        }
+    }
+
+    protected void initView(){}
+    protected void loadData(){}
+
+    protected abstract int getLayoutId();
+
+    protected View findView(int id){
+        return ViewUtil.findView(view,id);
+    }
+
+}
